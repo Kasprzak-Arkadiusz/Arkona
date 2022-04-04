47363a4335b85b8f5466d0a29dc9805d8ac1ed4c
Arkadiusz Kasprzak <Akasprzak016@gmail.com>
2022-04-04 14:33:07 +0200

    css files clean-up

--- src/client/src/pages/Home/components/OfferHolder.tsx
+++ src/client/src/pages/Home/components/OfferHolder.tsx
@@ -3,7 +3,7 @@
 function OfferHolder(props: { image: string; title: string }) {
     return (
         <div className="offer-holder">
-            <img src={props.image} alt="" />
+            <img className="offer-holder__image" src={props.image} alt="" />
             <span className="offer-holder__text">{props.title}</span>
         </div>
     );
