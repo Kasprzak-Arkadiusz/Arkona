fdb2e56ec3b692a0b5e24fc130a817b7708a8208
Arkadiusz Kasprzak <Akasprzak016@gmail.com>
2022-06-09 13:02:16 +0200

    Improve powershell script

--- scripts/Gen-Client.ps1
+++ scripts/Gen-Client.ps1
@@ -1,25 +1,55 @@
-﻿param(
-    [Parameter(Mandatory=$true)][string]$PROTO_FILE,
-    [string]$dir=$null
+﻿<#
+    .SYNOPSIS
+    Generates client-side .js and .ts files from .proto files
+    .PARAMETER PROTO_FILE_NAME
+    - If specifed generates files only for one provided .proto file.
+    - If not specifed generated files for ALL .proto files in folder
+    determined by PROTO_FILES_LOCATION parameter
+    .PARAMETER PROTO_FILES_LOCATION
+    Path to .proto files.
+    .PARAMETER OUT_DIR
+    Output directory where all files will be generated. 
+    .PARAMETER PROTOC_GEN_TS_ABS_PATH
+    Path to protoc-gen-ts.cmd script used to generate .js and .ts files. 
+    .EXAMPLE
+    .\scripts\Gen-Client.ps1 -PROTO_FILE_NAME greet.proto -PROTO_FILES_LOCATION "src\API\Protos" `
+    -OUT_DIR "src\client\src\generated" -PROTOC_GEN_TS_ABS_PATH "src\client\node_modules\.bin"
+    .NOTES
+        Created by    : Arkadiusz Kasprzak
+        Date Coded    : 09.06.2022
+        Last Modified : 09.06.2022
+#>
+
+param(
+    [Parameter(Mandatory = $false)][string]$PROTO_FILE_NAME = $null,
+    [Parameter(Mandatory = $false)][string]$PROTO_FILES_LOCATION = "src\API\Protos",
+    [Parameter(Mandatory = $false)][string]$OUT_DIR = "src\client\src\generated",
+    [Parameter(Mandatory = $false)][string]$PROTOC_GEN_TS_ABS_PATH = "src\client\node_modules\.bin"
 )
 
-if (!$dir)
+$protoFiles = if ($PROTO_FILE_NAME)
 {
-    $dotPos = $PROTO_FILE.LastIndexOf(".")
-    Write-Host "dotPos = $dotPos"
-    $slashPos = $PROTO_FILE.LastIndexOf("\")
-    Write-Host "slashPos = $slashPos"
-    $dir = $PROTO_FILE.Substring($slashPos + 1, $dotPos-$slashPos-1)
-    Write-Host $dir
+    $PROTO_FILE_NAME
+}
+else
+{
+    Get-ChildItem -Path $PROTO_FILES_LOCATION -Filter *.proto -Recurse -File -Name
 }
 
-$PROTOC_GEN_TS_PATH="C:\Users\PC\source\repos\Studia\Projekt inżynierski\Arkona\src\client\node_modules\.bin"
-$OUT_DIR="src\client\src\generated\$dir"
-$PROTO_FILES_LOCATION="src\API\Protos\"
+Write-Host "Generating files from the following .proto files:" -ForegroundColor Green
 
-New-Item -ItemType Directory -Force -Path $OUT_DIR | Out-Null
+foreach ($PROTO_FILE_NAME in $protoFiles)
+{
+    Write-Host "- $PROTO_FILE_NAME"
+    $dotPos = $PROTO_FILE_NAME.LastIndexOf(".")
+    $slashPos = $PROTO_FILE_NAME.LastIndexOf("\")
+    $DEST_FOLDER = $PROTO_FILE_NAME.Substring($slashPos + 1, $dotPos - $slashPos - 1)
 
-protoc --proto_path=$PROTO_FILES_LOCATION `
---plugin=protoc-gen-ts="$PROTOC_GEN_TS_PATH\protoc-gen-ts.cmd" `
---js_out="import_style=commonjs,binary:$OUT_DIR" `
---ts_out="service=grpc-web:$OUT_DIR" $PROTO_FILE
+    $FINAL_DIR = "$OUT_DIR\$DEST_FOLDER"
+    New-Item -ItemType Directory -Force -Path $FINAL_DIR | Out-Null
+
+    protoc --proto_path=$PROTO_FILES_LOCATION `
+    --plugin=protoc-gen-ts="$PROTOC_GEN_TS_ABS_PATH\protoc-gen-ts.cmd" `
+    --js_out="import_style=commonjs,binary:$FINAL_DIR" `
+    --ts_out="service=grpc-web:$FINAL_DIR" $PROTO_FILE_NAME
+}
