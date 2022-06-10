2397d6e2d14e8271849502a497ac6dc336022fa1
Arkadiusz Kasprzak <Akasprzak016@gmail.com>
2022-06-10 11:53:13 +0200

    Add lost files

---
+++ src/client/src/components/IconTitle/IconTitle.tsx
@@ -0,0 +1,18 @@
+﻿import SvgIcon from "assets/icons/Interfaces";
+import {TitleContainer, TitleSpan} from "./styled"
+
+interface Props {
+    Component: SvgIcon;
+    title: string;
+}
+
+function IconTitle({Component, title}: Props) {
+    return (
+        <TitleContainer>
+            <TitleSpan>{title}</TitleSpan>
+            {<Component/>}
+        </TitleContainer>
+    );
+}
+
+export default IconTitle;
