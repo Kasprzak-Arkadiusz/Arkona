f144d6e12aeacd22c2af5c6a47c95f3121df5563
Arkadiusz Kasprzak <Akasprzak016@gmail.com>
2022-11-19 16:38:44 +0100

    Add the image input to the add movie page

--- src/client/src/features/movies/add/AddMovie.tsx
+++ src/client/src/features/movies/add/AddMovie.tsx
@@ -1,13 +1,16 @@
-﻿import React from 'react';
-import SectionContainer from "components/SectionContainer/SectionContainer";
+﻿import React, {useState} from 'react';
 import * as style from "./styled";
 import AddSeances from "./AddSeances";
+import AddImage from "./components/AddImage/AddImage";
+import SectionContainer from "components/SectionContainer/SectionContainer";
 
 function AddMovie() {
+    const [image, setImage] = useState<string>("");
+
     return (
         <style.DisplayContainer>
             <SectionContainer>
-
+                <AddImage width={"360px"} height={"480px"} image={image} onImageChange={setImage}/>
             </SectionContainer>
             <AddSeances/>
         </style.DisplayContainer>
---
+++ src/client/src/features/movies/add/components/AddImage/AddImage.tsx
@@ -0,0 +1,36 @@
+﻿import React, {useRef} from 'react';
+import * as style from "./styled";
+
+interface Props {
+    width?: string,
+    height?: string,
+    image: string,
+    onImageChange: (imageBase64 :string) => void;
+}
+
+function AddImage({width = "220px", height = "320px", image, onImageChange}: Props) {
+    const imageRef = useRef<HTMLInputElement | null>(null);
+
+    const onImageClick = () => {
+        imageRef.current?.click();
+    }
+    
+    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files !== null && event.target.files[0] !== null) {
+            let reader = new FileReader();
+            reader.readAsDataURL(event.target.files[0]);
+            reader.onload = function () {
+                onImageChange(reader.result === null ? "" : reader.result.toString());
+            };
+        }
+    }
+    
+    return (
+        <style.Container width={width} height={height} onClick={onImageClick}> 
+            <style.Image src={image} alt="" title={"Dodaj zdjęcie"}/>
+            <style.ImageInput type="file" id="file" ref={imageRef} onChange={onChange}/>
+        </style.Container>
+    )
+}
+
+export default AddImage;
